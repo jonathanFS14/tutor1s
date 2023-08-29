74866689c9b10a5fbccf3ab2ba23facb3ea18030
jonathanFS14 <jona08p8@stud.kea.dk>
2023-08-29 12:41:26 +0200

    added bmi beregner

---
+++ src/BmiBeregner.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class BmiBeregner {
+    public static void main(String[] args) {
+
+        double højde;
+        double vægt;
+        double bmi;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Her kan du tjekke din BMI du skal blot indtaste dine værdier nedenunder");
+        System.out.println("Indtast din højde i meter: ");
+
+        højde = sc.nextDouble();
+
+        if (højde > 3){
+            højde = (højde/100);
+        }
+
+        System.out.println("indtast din vægt i Kg: ");
+
+        vægt = sc.nextDouble();
+
+        bmi = vægt / (højde * højde);
+
+        System.out.println("Dit BMI er " + bmi);
+
+        if (bmi < 18.5){
+            System.out.println("Du er undervægtigt, spis noget mere");
+        }
+
+        if (bmi >= 25){
+            System.out.println("Du er overvægtig, gå ud og løb en tur");
+        }
+
+        else if (bmi >= 18.5 && bmi < 25) {
+            System.out.println("Du har en normal vægt");
+        }
+
+    }
+}
--- src/taxiberegner/TaxiTur.java
+++ src/taxiberegner/TaxiTur.java
@@ -1,3 +1,5 @@
+package taxiberegner;
+
 import java.util.Scanner;
 
 public class TaxiTur {
